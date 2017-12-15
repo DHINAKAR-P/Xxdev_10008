6dffff67843692643de32e3e9e4344fe1fc6fd67
DHINAKAR P <dhinakarintech@gmail.com>
2017-12-15 12:28:40 +0530

    exec - tablet file location changed
    exec - tablet file location changed

--- projects/Tablet/ios/exec_tablet.sh
+++ projects/Tablet/ios/exec_tablet.sh
@@ -1,7 +1,7 @@
 export PATH=$PATH:/Users/administrator/gradle-2.11/bin
 echo 'Gradle Path set..!'
 echo g5eBCuKV | sudo -S chmod -R 777 ~/Desktop/Xxdev_10008/
-cd ~/Desktop/Xxdev_10008/
+cd ~/Desktop/Xxdev_10008/Xxdev_10008/projects/Tablet/ios/
 pwd
 echo 'Started to excute gradle script'
 gradle -q deploytabletIPA
