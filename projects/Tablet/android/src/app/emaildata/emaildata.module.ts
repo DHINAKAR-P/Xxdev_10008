e4ca2d5934709ac76be3c5fda39850c444b0229e
geppetto <geppetto@bebe048f8d40>
2017-12-15 06:06:54 +0000

    Committing my code

--- projects/Tablet/android/src/app/emaildata/emaildata.module.ts
+++ projects/Tablet/android/src/app/emaildata/emaildata.module.ts
@@ -4,28 +4,23 @@ import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabletEmailCreationengComponent } from './tabletemailcreationeng.component';
 import { UpdateCreationengComponent } from './updatecreationeng.component';
-import { ListScreenengComponent } from './listscreeneng.component';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material';
-import { AgGridModule } from 'ag-grid-angular/main';
 import { AgGridModule } from 'ag-grid-angular/main';
 import { EmaildataService } from './emaildata.service';
 
 @NgModule({
     declarations: [
         TabletEmailCreationengComponent,
-        UpdateCreationengComponent,
-        ListScreenengComponent
+        UpdateCreationengComponent
     ],
     imports: [
         FormsModule,
         MatInputModule,
         AgGridModule.withComponents([]),
-        AgGridModule.withComponents([]),
         RouterModule.forChild([
             { path: 'TabletEmailCreationeng', component: TabletEmailCreationengComponent},
-            { path: 'UpdateCreationeng', component: UpdateCreationengComponent},
-            { path: 'ListScreeneng', component: ListScreenengComponent}
+            { path: 'UpdateCreationeng', component: UpdateCreationengComponent}
         ]),
         Ng2Bs3ModalModule,
         BrowserAnimationsModule
